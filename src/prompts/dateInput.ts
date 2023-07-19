6f99fecbb5fede7b071dc8897794bc6b74dad90b
VitorDaynno <vitordaynno@gmail.com>
2023-07-18 23:49:45 -0300

    Adding mask and validation on dateInput

--- src/prompts/dateInput.ts
+++ src/prompts/dateInput.ts
@@ -6,9 +6,18 @@ import { isValidDate } from '../helpers/date';
 export const dateInput = async (message: string): Promise<string> => {
   const text = await input({
     message,
-    validate: (value = '') => isValidDate(value, 'dd/MM/yyyy')
+    validate: (value = '') => isValidDate(formatValue(value), 'dd/MM/yyyy')
       || 'Insira uma data válida',
+    transformer: (value) => formatValue(value),
   });
 
-  return text;
+  return formatValue(text);
+};
+
+const formatValue = (text: string) => {
+  const quantityToComplete = text.length < 8 ? 8 - text.length : 0;
+
+  return ('0'.repeat(quantityToComplete) + text)
+    .substring(text.length - 8)
+    .replace(/(^\d{2})(\d{2})(\d{4}$)/g, '$1/$2/$3');
 };
